34ab54d9531776d37971fa2ca1db6826da41737e
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 12:13:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
-st.title("Upload + Classification Example")
+st.title("SharkNet: Automatic Breast Cancer Classification from Thermography")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-st.image(uploaded_file)
+st.image(
+    uploaded_file, caption=f"Original image", use_column_width=True,
+)
+
+#process the image thru the stuff
+salient_image = uploaded_file
+
+probability = 0
+st.write("Probability of tumor presence: " + str(probability) + "%.")
+st.write("Processed Image")
+st.write("Red dots indicate points of interest.")
+st.image(
+    salient_image, caption=f"Processed image", use_column_width=True,
+)
