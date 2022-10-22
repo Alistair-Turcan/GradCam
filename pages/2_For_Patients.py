3bceb9e4d3c7e239d85b03a5ecb2b0c24d50652f
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 12:49:16 -0400

    Update 2_For_Patients.py

--- pages/2_For_Patients.py
+++ pages/2_For_Patients.py
@@ -1,1 +1,7 @@
 
+import streamlit as st
+st.set_page_config(page_title="For Patients", page_icon="📈")
+st.sidebar.header("For Patients")
+st.title("For Patients")
+
+st.video('https://www.youtube.com/watch?v=MPSz7UKbbOQ')
