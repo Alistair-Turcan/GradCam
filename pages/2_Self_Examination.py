70cde81bdb0c59d28f7ad6289cd65e38c48d8e82
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 13:23:25 -0400

    Update 2_Self_Examination.py

--- pages/2_Self_Examination.py
+++ pages/2_Self_Examination.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-st.set_page_config(page_title="Self Examination", page_icon="📈")
+st.set_page_config(page_title="Self Examination")
 st.sidebar.header("Self Examination")
 st.title("Self Examination")
 
