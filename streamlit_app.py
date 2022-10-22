0e60130d303a3ae1d0b741ce3b2eeac473c1eab9
Alistair-Turcan <alistairturcan@gmail.com>
2022-10-22 14:22:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.title("SharkNet: Automatic Breast Cancer Classification from Thermography")
 st.set_page_config(
-    page_title="SharkNet",
-    page_icon="👋",
+    page_title="SharkNet"
 )
 
+st.sidebar.success("Select a page above.")
 
